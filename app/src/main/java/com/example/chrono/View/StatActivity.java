c40a2ce1d7052bb5f2fb2906b7ef50c669a31496
christ3444 <christianmonmail34444@gmail.com>
2022-01-22 18:24:19 +0000

    create statistique activity

---
+++ app/src/main/java/com/example/chrono/View/StatActivity.java
@@ -0,0 +1,16 @@
+package com.example.chrono.View;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.chrono.R;
+
+public class StatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stat);
+    }
+}
